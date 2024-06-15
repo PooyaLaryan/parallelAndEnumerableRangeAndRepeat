16334f0654603f5698faa38bb4d9cb9769015e23
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-06-15 08:34:56 +0330

    Add project files.

---
+++ parallelForEachSample/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Parallel.ForEach(Enumerable.Range(1, 10), x =>
+{
+    Console.WriteLine(x);
+});
+
+
+Parallel.ForEach(Enumerable.Repeat(1, 10), (x,y) =>
+{
+    Console.WriteLine(x);
+});
